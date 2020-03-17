f6f7680f0c800ba836a857da2f0428e451be6bd0
Corey Alexander <coreyja@gmail.com>
2020-03-17 16:46:32 -0400

    Extract validator and plug it into the writer

---
+++ app/models/journaled/validator.rb
@@ -0,0 +1,58 @@
+class Journaled::Validator
+  REQUIRED_EVENT_METHOD_NAMES = %i(
+    journaled_schema_name
+    journaled_partition_key
+    journaled_attributes
+    journaled_app_name
+    journaled_enqueue_opts
+  ).freeze
+
+  def initialize(journaled_event:)
+    @journaled_event = journaled_event
+  end
+
+  def validate_required_methods!
+    unless respond_to_all?(journaled_event, REQUIRED_EVENT_METHOD_NAMES)
+      raise "An enqueued event must respond to: #{REQUIRED_EVENT_METHOD_NAMES.to_sentence}"
+    end
+
+    unless journaled_event.journaled_schema_name.present? &&
+        journaled_event.journaled_partition_key.present? &&
+        journaled_event.journaled_attributes.present?
+      raise <<~ERROR
+        An enqueued event must have a non-nil response to:
+          #json_schema_name,
+          #partition_key, and
+          #journaled_attributes
+      ERROR
+    end
+  end
+
+  def validate_serialized_event!
+    base_event_json_schema_validator.validate! serialized_event
+    json_schema_validator.validate! serialized_event
+  end
+
+  private
+
+  attr_reader :journaled_event
+  delegate(*REQUIRED_EVENT_METHOD_NAMES, to: :journaled_event)
+
+  def serialized_event
+    @serialized_event ||= journaled_attributes.to_json
+  end
+
+  def json_schema_validator
+    @json_schema_validator ||= Journaled::JsonSchemaModel::Validator.new(journaled_schema_name)
+  end
+
+  def base_event_json_schema_validator
+    @base_event_json_schema_validator ||= Journaled::JsonSchemaModel::Validator.new('base_event')
+  end
+
+  def respond_to_all?(object, method_names)
+    method_names.all? do |method_name|
+      object.respond_to?(method_name)
+    end
+  end
+end
--- app/models/journaled/writer.rb
+++ app/models/journaled/writer.rb
@@ -1,63 +1,30 @@
 class Journaled::Writer
-  EVENT_METHOD_NAMES = %i(
-    journaled_schema_name
-    journaled_partition_key
-    journaled_attributes
-    journaled_app_name
-    journaled_enqueue_opts
-  ).freeze
-
   def initialize(journaled_event:)
-    raise "An enqueued event must respond to: #{EVENT_METHOD_NAMES.to_sentence}" unless respond_to_all?(journaled_event, EVENT_METHOD_NAMES)
-
-    unless journaled_event.journaled_schema_name.present? &&
-        journaled_event.journaled_partition_key.present? &&
-        journaled_event.journaled_attributes.present?
-      raise <<~ERROR
-        An enqueued event must have a non-nil response to:
-          #json_schema_name,
-          #partition_key, and
-          #journaled_attributes
-      ERROR
-    end
-
     @journaled_event = journaled_event
+
+    validator.validate_required_methods!
   end
 
   def journal!
-    base_event_json_schema_validator.validate! serialized_event
-    json_schema_validator.validate! serialized_event
+    validator.validate_serialized_event!
+
     Journaled.enqueue!(journaled_delivery, journaled_enqueue_opts)
   end
 
   private
 
   attr_reader :journaled_event
-  delegate(*EVENT_METHOD_NAMES, to: :journaled_event)
+  delegate :journaled_attributes, :journaled_partition_key, :journaled_app_name, :journaled_enqueue_opts, to: :journaled_event
 
   def journaled_delivery
-    @journaled_delivery ||= Journaled::Delivery.new(
-      serialized_event: serialized_event,
+    Journaled::Delivery.new(
+      serialized_event: journaled_attributes.to_json,
       partition_key: journaled_partition_key,
       app_name: journaled_app_name,
     )
   end
 
-  def serialized_event
-    @serialized_event ||= journaled_attributes.to_json
-  end
-
-  def json_schema_validator
-    @json_schema_validator ||= Journaled::JsonSchemaModel::Validator.new(journaled_schema_name)
-  end
-
-  def base_event_json_schema_validator
-    @base_event_json_schema_validator ||= Journaled::JsonSchemaModel::Validator.new('base_event')
-  end
-
-  def respond_to_all?(object, method_names)
-    method_names.all? do |method_name|
-      object.respond_to?(method_name)
-    end
+  def validator
+    @validator ||= Journaled::Validator.new(journaled_event: journaled_event)
   end
 end
