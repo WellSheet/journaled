5033b6ee65f27e945af419bdeae0676070cd35f4
Corey Alexander <coreyja@gmail.com>
2020-03-17 17:19:50 -0400

    Allow passing in some opts for usability

--- app/models/journaled/bulk_writer.rb
+++ app/models/journaled/bulk_writer.rb
@@ -1,11 +1,12 @@
 class Journaled::BulkWriter
-  def initialize(journaled_events:, app_name:)
+  def initialize(journaled_events:, app_name:, enqueue_opts: {})
     journaled_events.each do |event|
       Journaled::Validator.new(journaled_event: event).validate_required_methods!
     end
 
     @journaled_events = journaled_events
     @app_name = app_name
+    @enqueue_opts = enqueue_opts
   end
 
   def journal!
@@ -13,12 +14,12 @@ class Journaled::BulkWriter
       Journaled::Validator.new(journaled_event: event).validate_serialized_event!
     end
 
-    Journaled.enqueue!(journaled_delivery, {})
+    Journaled.enqueue!(journaled_delivery, enqueue_opts)
   end
 
   private
 
-  attr_reader :journaled_events, :app_name
+  attr_reader :journaled_events, :app_name, :enqueue_opts
 
   def journaled_delivery
     Journaled::BulkDelivery.new(
