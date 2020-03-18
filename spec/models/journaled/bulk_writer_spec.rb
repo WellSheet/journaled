ccb92a19ab3ca441d04665319c647e6f52812639
Corey Alexander <coreyja@gmail.com>
2020-03-18 15:30:06 -0400

    Get bulk writer spec moved over to use serializer

--- spec/models/journaled/bulk_writer_spec.rb
+++ spec/models/journaled/bulk_writer_spec.rb
@@ -1,186 +1,103 @@
 require 'rails_helper'
 
 RSpec.describe Journaled::BulkWriter do
-  let(:enqueue_opts) { {} }
-  let(:journaled_events) { [journaled_event] }
-  subject { described_class.new journaled_events: journaled_events, app_name: "my_app", enqueue_opts: enqueue_opts }
-
-  describe '#initialize' do
-    context 'when the Journaled Event complies with the API' do
-      let(:journaled_event) do
-        double(
-          journaled_schema_name: :fake_schema_name,
-          journaled_attributes: { foo: :bar },
-          journaled_partition_key: 'fake_partition_key',
-          journaled_app_name: nil,
-          journaled_enqueue_opts: {},
-        )
-      end
-
-      it 'does not raise on initialization' do
-        expect { subject }.not_to raise_error
-      end
-    end
-  end
-
   describe '#journal!' do
-    let(:schema_path) { Journaled::Engine.root.join "journaled_schemas/fake_schema_name.json" }
-    let(:schema_file_contents) do
-      <<-JSON
-          {
-            "title": "Foo",
-            "type": "object",
-            "properties": {
-              "foo": {
-                "type": "string"
-              }
-            },
-            "required": ["foo"]
-          }
-      JSON
+    let(:enqueue_opts) { {} }
+
+    let(:journaled_event_1) do
+      double
+    end
+    let(:journaled_event_2) do
+      double
     end
 
+    let(:journaled_events) { [journaled_event_1, journaled_event_2] }
+    subject { described_class.new journaled_events: journaled_events, app_name: "my_app", enqueue_opts: enqueue_opts }
+
+    let(:stub_serializer_1) do
+      instance_double(Journaled::Serializer, serialize!: true, serialized_event: 'FAKE_SERIALIZED_EVENT_1', journaled_partition_key: 'key_1')
+    end
+    let(:stub_serializer_2) do
+      instance_double(Journaled::Serializer, serialize!: true, serialized_event: 'FAKE_SERIALIZED_EVENT_2', journaled_partition_key: 'key_2')
+    end
     before do
-      allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?).with(schema_path).and_return(true)
-      allow(File).to receive(:read).and_call_original
-      allow(File).to receive(:read).with(schema_path).and_return(schema_file_contents)
-    end
-
-    let(:journaled_enqueue_opts) { {} }
-    let(:journaled_event) do
-      double(
-        journaled_schema_name: :fake_schema_name,
-        journaled_attributes: journaled_event_attributes,
-        journaled_partition_key: 'fake_partition_key',
-        journaled_app_name: 'my_app',
-        journaled_enqueue_opts: journaled_enqueue_opts,
-      )
+      allow(Journaled::Serializer).to receive(:new).with(journaled_event: journaled_event_1).and_return(stub_serializer_1)
+      allow(Journaled::Serializer).to receive(:new).with(journaled_event: journaled_event_2).and_return(stub_serializer_2)
     end
 
     around do |example|
       with_jobs_delayed { example.run }
     end
 
-    context 'when the Journaled Event does not implement all the necessary methods' do
-      let(:journaled_event) { double }
+    context 'when one of the events does not serialize correctly' do
+      let(:stub_serializer_1) { instance_double(Journaled::Serializer, serialize!: true, serialized_event: nil) }
+      before do
+        allow(stub_serializer_1).to receive(:serialize!).and_raise('An enqueued event must respond to: journaled_attributes')
+      end
 
-      it 'raises' do
+      it 'raises and does not enqueue anything' do
         expect { subject.journal! }.to raise_error RuntimeError, /An enqueued event must respond to/
+        expect(Delayed::Job.count).to eq 0
       end
     end
 
-    context 'when the Journaled Event returns non-present values for some of the required methods' do
-      let(:journaled_event) do
-        double(
-          journaled_schema_name: nil,
-          journaled_attributes: {},
-          journaled_partition_key: '',
-          journaled_app_name: nil,
-          journaled_enqueue_opts: {},
+    context 'when all the events are serializable' do
+      it 'enqueues a delivery with the correct params' do
+        allow(Journaled::BulkDelivery).to receive(:new).and_call_original
+        expect { subject.journal! }.to change {
+          Delayed::Job.where('handler like ?', '%Journaled::BulkDelivery%').count
+        }.from(0).to(1)
+
+        expect(Journaled::BulkDelivery).to have_received(:new)
+          .with(
+            app_name: 'my_app',
+            serialized_events: %w(FAKE_SERIALIZED_EVENT_1 FAKE_SERIALIZED_EVENT_2),
+            partition_keys: %w(key_1 key_2),
         )
       end
 
-      it 'raises' do
-        expect { subject.journal! }.to raise_error RuntimeError, /An enqueued event must have a non-nil response to/
-      end
-    end
-
-    context 'when the journaled event does NOT comply with the base_event schema' do
-      let(:journaled_event_attributes) { { foo: 1 } }
-
-      it 'raises an error and does not enqueue anything' do
-        expect { subject.journal! }.to raise_error JSON::Schema::ValidationError
-        expect(Delayed::Job.where('handler like ?', '%Journaled%').count).to eq 0
-      end
-    end
-
-    context 'when the event complies with the base_event schema' do
-      context 'when the specific json schema is NOT valid' do
-        let(:journaled_event_attributes) { { id: 'FAKE_UUID', event_type: 'fake_event', created_at: Time.zone.now, foo: 1 } }
-
-        it 'raises an error and does not enqueue anything' do
-          expect { subject.journal! }.to raise_error JSON::Schema::ValidationError
-          expect(Delayed::Job.where('handler like ?', '%Journaled%').count).to eq 0
+      context 'when the number of events passed in exceeds EVENTS_PER_DELIVERY' do
+        before do
+          stub_const("#{described_class}::EVENTS_PER_DELIVERY", 1)
         end
-      end
 
-      context 'when the specific json schema is also valid' do
-        let(:journaled_event_attributes) { { id: 'FAKE_UUID', event_type: 'fake_event', created_at: Time.zone.now, foo: :bar } }
-
-        it 'creates a delivery with the app name passed through' do
+        it 'creates multiple deliveries' do
           allow(Journaled::BulkDelivery).to receive(:new).and_call_original
           expect { subject.journal! }.to change {
             Delayed::Job.where('handler like ?', '%Journaled::BulkDelivery%').count
+          }.from(0).to(2)
+
+          expect(Journaled::BulkDelivery).to have_received(:new)
+            .with(hash_including(app_name: 'my_app')).twice
+          expect(Journaled::BulkDelivery).to have_received(:new)
+            .with(app_name: 'my_app', serialized_events: %w(FAKE_SERIALIZED_EVENT_1), partition_keys: %w(key_1)).once
+          expect(Journaled::BulkDelivery).to have_received(:new)
+            .with(app_name: 'my_app', serialized_events: %w(FAKE_SERIALIZED_EVENT_2), partition_keys: %w(key_2)).once
+        end
+      end
+
+      context 'when there is no job priority specified in the enqueue opts' do
+        around do |example|
+          old_priority = Journaled.job_priority
+          Journaled.job_priority = 999
+          example.run
+          Journaled.job_priority = old_priority
+        end
+
+        it 'defaults to the global default' do
+          expect { subject.journal! }.to change {
+            Delayed::Job.where('handler like ?', '%Journaled::BulkDelivery%').where(priority: 999).count
           }.from(0).to(1)
-
-          expect(Journaled::BulkDelivery).to have_received(:new).with(hash_including(app_name: 'my_app'))
         end
+      end
 
-        context 'when there is no job priority specified in the enqueue opts' do
-          around do |example|
-            old_priority = Journaled.job_priority
-            Journaled.job_priority = 999
-            example.run
-            Journaled.job_priority = old_priority
-          end
+      context 'when there is a job priority specified in the enqueue opts' do
+        let(:enqueue_opts) { { priority: 13 } }
 
-          it 'defaults to the global default' do
-            expect { subject.journal! }.to change {
-              Delayed::Job.where('handler like ?', '%Journaled::BulkDelivery%').where(priority: 999).count
-            }.from(0).to(1)
-          end
-        end
-
-        context 'when there is a job priority specified in the enqueue opts' do
-          let(:enqueue_opts) { { priority: 13 } }
-
-          it 'enqueues a Journaled::Delivery object with the given priority' do
-            expect { subject.journal! }.to change {
-              Delayed::Job.where('handler like ?', '%Journaled::BulkDelivery%').where(priority: 13).count
-            }.from(0).to(1)
-          end
-        end
-
-        context 'when multiple valid events are passed in' do
-          let(:journaled_event_attributes_2) { { id: 'FAKE_UUID_2', event_type: 'fake_event', created_at: Time.zone.now, foo: :bar } }
-          let(:journaled_event_2) do
-            double(
-              journaled_schema_name: :fake_schema_name,
-              journaled_attributes: journaled_event_attributes_2,
-              journaled_partition_key: 'fake_partition_key',
-              journaled_app_name: 'my_app',
-              journaled_enqueue_opts: journaled_enqueue_opts,
-            )
-          end
-
-          let(:journaled_events) { [journaled_event, journaled_event_2] }
-
-          it 'creates a delivery' do
-            allow(Journaled::BulkDelivery).to receive(:new).and_call_original
-            expect { subject.journal! }.to change {
-              Delayed::Job.where('handler like ?', '%Journaled::BulkDelivery%').count
-            }.from(0).to(1)
-
-            expect(Journaled::BulkDelivery).to have_received(:new)
-              .with(hash_including(app_name: 'my_app'))
-          end
-
-          context 'when the number of events passed in exceeds EVENTS_PER_DELIVERY' do
-            before do
-              stub_const("#{described_class}::EVENTS_PER_DELIVERY", 1)
-            end
-
-            it 'creates multiple deliveries' do
-              allow(Journaled::BulkDelivery).to receive(:new).and_call_original
-              expect { subject.journal! }.to change {
-                Delayed::Job.where('handler like ?', '%Journaled::BulkDelivery%').count
-              }.from(0).to(2)
-
-              expect(Journaled::BulkDelivery).to have_received(:new)
-                .with(hash_including(app_name: 'my_app')).twice
-            end
-          end
+        it 'enqueues a Journaled::Delivery object with the given priority' do
+          expect { subject.journal! }.to change {
+            Delayed::Job.where('handler like ?', '%Journaled::BulkDelivery%').where(priority: 13).count
+          }.from(0).to(1)
         end
       end
     end
