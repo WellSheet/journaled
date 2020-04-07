e4d6b60ab40f93d17d1684b3692fba6d6c2ee29c
Corey Alexander <corey@wellsheet.com>
2020-04-07 13:25:27 -0400

    Bulk Delivery Error Improvements (#5)
    * Very messy but raise a correct error type. Realized there are only 2 sub-types so easier than I thought I think
    
    * Get some better errors raised when all records fail. Need to think about individual cases
    
    * Refactor into an inner class that passes Rubocop
    
    * Rename method for potential clarity
    
    * Get rid of small var name

--- app/models/journaled/bulk_delivery.rb
+++ app/models/journaled/bulk_delivery.rb
@@ -8,7 +8,7 @@ class Journaled::BulkDelivery
     return unless Journaled.enabled?
 
     response = kinesis_client.put_records request
-    requeue_failed_records!(response)
+    handle_failures!(response)
   rescue Aws::Kinesis::Errors::InternalFailure, Aws::Kinesis::Errors::ServiceUnavailable, Aws::Kinesis::Errors::Http503Error => e
     Rails.logger.error "Kinesis Error - Server Error occurred - #{e.class}"
     raise KinesisTemporaryFailure
@@ -41,17 +41,95 @@ class Journaled::BulkDelivery
     Journaled::KinesisClient.generate
   end
 
-  def requeue_failed_records!(response) # rubocop:disable Metrics/AbcSize
-    records_with_responses = records.zip(response.records)
-    errored_records = records_with_responses.select { |_record, resp| resp.error_code.present? }.map(&:first)
+  def handle_failures!(response)
+    ErrorHandler.new(response, records, app_name).handle!
+  end
 
-    failed_record_count = response.failed_record_count || 0
-    raise 'FailedRecordCount differs from count of records that have errors' unless errored_records.count == failed_record_count
-    raise 'ALL Records failed to be added to the Kinesis steam' if errored_records.count == records.count
+  class ErrorHandler
+    attr_reader :response, :records, :app_name
 
-    Delayed::Job.enqueue self.class.new(records: errored_records, app_name: app_name) if errored_records.any?
+    def initialize(response, records, app_name)
+      @response = response
+      @records = records
+      @app_name = app_name
+    end
+
+    def handle!
+      handle_failed_record_count_mismatch!
+      handle_all_records_failed!
+      renenqueue_failed_records!
+    end
+
+    private
+
+    def handle_failed_record_count_mismatch!
+      failed_record_count = response.failed_record_count || 0
+
+      unless errored_records_with_responses.count == failed_record_count
+        raise 'FailedRecordCount differs from count of records that have errors'
+      end
+    end
+
+    def handle_all_records_failed!
+      return unless errored_records.count == records.count
+
+      if error_objects.count == 1
+        raise(error_objects.first)
+      else
+        raise MultipleErrorsFailure, error_objects
+      end
+    end
+
+    def renenqueue_failed_records!
+      Delayed::Job.enqueue Journaled::BulkDelivery.new(records: errored_records, app_name: app_name) if errored_records.any?
+    end
+
+    def error_objects
+      @error_objects ||= begin
+                           grouped_errors = errors.group_by(&:error_code).to_a
+
+                           grouped_errors.map do |error_code, group|
+                             error_class_for_code(error_code).new(group.map(&:error_message).join("\n"))
+                           end
+                         end
+    end
+
+    def error_class_for_code(error_code)
+      if error_code == 'ProvisionedThroughputExceededException'
+        KinesisBulkRateLimitFailure
+      else
+        KinesisBulkInternalErrorFailure
+      end
+    end
+
+    def errored_records
+      errored_records_with_responses.map(&:first)
+    end
+
+    def errors
+      errored_records_with_responses.map(&:second)
+    end
+
+    def errored_records_with_responses
+      @errored_records_with_responses ||= records_with_responses.select { |_record, resp| resp.error_code.present? }
+    end
+
+    def records_with_responses
+      @records_with_responses ||= records.zip(response.records)
+    end
   end
 
   class KinesisTemporaryFailure < Journaled::NotTrulyExceptionalError
+  end
+  class KinesisBulkInternalErrorFailure < Journaled::NotTrulyExceptionalError
+  end
+  class KinesisBulkRateLimitFailure < StandardError
+  end
+
+  class MultipleErrorsFailure < StandardError
+    def initialize(errors)
+      msgs = errors.map { |e| "#{e.class}\n#{e.message}" }
+      super(msgs.join("\n\n"))
+    end
   end
 end
